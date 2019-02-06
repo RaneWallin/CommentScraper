e069de08159b1c1f53076f1958c123ffb2daf024
Rane Wallin <RAN2143066@maricopa.edu>
2019-02-06 03:38:08 -0700

    Setting up GUI

--- src/Constants.java
+++ src/Constants.java
@@ -1,9 +1,11 @@
 public interface Constants {
-    public static final int SCENE_WIDTH = 700;
-    public static final int SCENE_HEIGHT = 300;
-    public static final int VBOX_WIDTH = 300;
-    public static final int VBOX_HEIGHT = 200;
-    public static final int MAIN_GUI_SPACING = 50;
+    public static final int SCENE_WIDTH = 800;
+    public static final int SCENE_HEIGHT = 400;
+    public static final int VBOX_WIDTH = 365;
+    public static final int VBOX_HEIGHT = 300;
+    public static final int MAIN_GUI_SPACING = 25;
+
 
     public static final String GUI_TITLE = "Comment Scraper";
+    public static final String START_BUTTON_TXT = "Start";
 }
