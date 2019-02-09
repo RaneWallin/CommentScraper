206baf0570d74f13f88b0eddee057b3ccc3aacbe
Rane Wallin <RAN2143066@maricopa.edu>
2019-02-09 12:18:46 -0700

    completed adding GUI elements

--- src/Constants.java
+++ src/Constants.java
@@ -1,16 +1,27 @@
 public interface Constants {
-    public static final int SCENE_WIDTH = 800;
-    public static final int SCENE_HEIGHT = 400;
-    public static final int VBOX_WIDTH = 365;
-    public static final int VBOX_HEIGHT = 300;
-    public static final int MAIN_GUI_SPACING = 25;
+    // Dimensions
+    int MAIN_GUI_SPACING = 25;
 
 
-    public static final String GUI_TITLE = "CScrape: Comment Scraper";
-    public static final String START_BUTTON_TXT = "Start";
-    public static final String DEFAULT_FONT = "Arial";
-    public static final String INPUT_INSTRUCTIONS = "Enter filename";
-    public static final String CHOOSER_BUTTON_TXT = "Find";
-    public static final String BLANK_ROW = "";
-    public static final String BLANK_COLUMN = BLANK_ROW;
+    // Strings
+    String GUI_TITLE = "CScrape: Comment Scraper";
+    String INPUT_INSTRUCTIONS = "Enter filename";
+    String OUTPUT_COMPLETE_TXT = "Your new file has been created.  \nClick \"Open\" to view it.";
+
+    // Button text
+    String START_BUTTON_TXT = "Start";
+    String CHOOSER_BUTTON_TXT = "Find";
+    String OPEN_BUTTON_TXT = "Open";
+
+    // styles and formatting
+    String GRAY_BG = "-fx-background-color: lightgray; ";
+    String SMOKE_BG = "-fx-background-color: whitesmoke; ";
+    String BLACK_BORDER = "-fx-border-color: black; ";
+    String PADDING5PX = "-fx-padding: 5";
+    String HIDDEN = "-fx-opacity: 0";
+    String DEFAULT_FONT = "Arial";
+    String BLANK_ROW = "";
+    String BLANK_COLUMN = BLANK_ROW;
+
+    String LOREM = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 }
